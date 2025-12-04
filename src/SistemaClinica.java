960a82cf507c0e9fe3f41858f16edba02f030b67
Maku <maku_serrano@outlook.com>
2025-12-04 19:00:08 -0300

    falta el main

--- src/SistemaClinica.java
+++ src/SistemaClinica.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SistemaClinica {
@@ -6,14 +7,16 @@ public class SistemaClinica {
     private HashMap<Integer, String> consultoriosOcupados;
     // numero consult /// codigo del turno
     static final int TOTAL_CONSULTORIOS = 20;
-    public SistemaClinica(){
+
+    public SistemaClinica() {
         this.turnos = new HashMap<>();
         this.consultoriosOcupados = new HashMap<>();
     }
+
     public void registrarTurno(Turno turno) throws IllegalStateException {
         int numConsul = turno.getNumeroConsultorio();
         if (consultoriosOcupados.containsKey(numConsul)) {
-            throw new IllegalStateException("El consultorio" + numConsul + " ya esta ocupado" );
+            throw new IllegalStateException("El consultorio" + numConsul + " ya esta ocupado");
         }
         turnos.put(turno.getCodigoTurno(), turno);
         consultoriosOcupados.put(numConsul, turno.getCodigoTurno());
@@ -26,17 +29,65 @@ public class SistemaClinica {
         if (codigoTurno == null) {
             throw new TurnoNoEncontradoException("Turno " + codigoTurno + " no encontrado");
         }
-        
+
         t.setEstado(EstadoTurno.CONFIRMADO);
-        
+
     }
 
     public void registrarIngresoPaciente(String codigoTurno) throws TurnoNoEncontradoException{
         Turno t = turnos.get(codigoTurno);
-        if (turnos.containsKey(codigoTurno)) {
-            throw new TurnoNoEncontradoException("El consultorio " + codigoTurno + " ya esta asignado a otro turno")
+        if (!turnos.containsKey(codigoTurno)) {
+            throw new TurnoNoEncontradoException("El consultorio " + codigoTurno + " ya esta asignado a otro turno");
             
         }
         t.setEstado(EstadoTurno.EN_CURSO);
+    }
+
+    public void finalizarTurno(String codigoTurno) throws TurnoNoEncontradoException {
+
+        if (!turnos.containsKey(codigoTurno)) {
+            throw new TurnoNoEncontradoException("Turno no existente");
+        }
+
+        Turno t = turnos.get(codigoTurno);
+        t.setEstado(EstadoTurno.FINALIZADO);
+        consultoriosOcupados.remove(t.getNumeroConsultorio());
+    }
+
+    public void cancelarTurno(String codigoTurno) throws TurnoNoEncontradoException {
+        if (!turnos.containsKey(codigoTurno)) {
+            throw new TurnoNoEncontradoException("No existe el turno.");
+        }
+        Turno t = turnos.get(codigoTurno);
+        t.setEstado(EstadoTurno.CANCELADO);
+        consultoriosOcupados.remove(t.getNumeroConsultorio());
+    }
+
+    public Turno buscarTurnoPorCodigo(String codigoTurno) throws TurnoNoEncontradoException {
+        if (codigoTurno == null) {
+            throw new TurnoNoEncontradoException("Turno" + codigoTurno + " no encontrado");
+        }
+        Turno t = turnos.get(codigoTurno);
+        return t;
+    }
+
+    public ArrayList<EstadoTurno> obtenerEstadoDeTurnos(EstadoTurno estado){
+        ArrayList<EstadoTurno> arraydeturnos = new ArrayList<>();
+
+        for(Turno t : turnos.values()){
+            if ( t.getEstado() == estado) {
+                arraydeturnos.add(estado);
+            }
+        }
+        return arraydeturnos;
+    }
+
+    public String generarReporteOcupacion(){
+        int  totalTurnos = turnos.size();
+        int consultoriosOcup = consultoriosOcupados.size();
+        double ocupacion = (consultoriosOcup / TOTAL_CONSULTORIOS) * 100;
+        return "Total turnos: " + totalTurnos + "\n" +
+            "Consultorios ocupados: " + consultoriosOcup + "\n" +
+            "Ocupacion: " + ocupacion + "%";
     }
 }
